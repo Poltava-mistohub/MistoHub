19558e7312332366dcd77e17adeb725cde1f33e8
Poltava-mistohub <mistohub@gmail.com>
2024-06-12 09:33:43 +0000

    Deploying to gh-pages from @ Poltava-mistohub/MistoHub@13b754ea1a03637d3f6a7c9f6091457790ade1b9 🚀

--- assets/InvestorsBlock-f929bbf0.js
+++ assets/InvestorsBlock-f929bbf0.js
@@ -1,4 +1,4 @@
-import{u as h,j as t,f as r,h as A,r as x}from"./index-802f1575.js";import{M,I as $,u as j,n as y,C as W,g as N}from"./HomePage-0324bf2b.js";const I=h.div`
+import{u as h,j as t,f as r,h as A,r as x}from"./index-196508c4.js";import{M,I as $,u as j,n as y,C as W,g as N}from"./HomePage-99e5b234.js";const I=h.div`
   .text {
     color: rgba(11, 11, 11, 0.5);
     font-size: 12px;
