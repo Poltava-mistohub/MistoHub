a3419fc50cc18ea7fdcc62d1ec06ef410dc2aaa9
SerhiiMonzhelesov <129993794+SerhiiMonzhelesov@users.noreply.github.com>
2024-06-13 11:55:41 +0300

    change loading for main image

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -46,26 +46,26 @@ const HeroSection = () => {
                 <source
                   srcSet={`${item.mobile.webp} 1x, ${item.mobile.webp_2x} 2x`}
                   media="(max-width: 767px)"
-                  sizes='(max-width: 767px) 375px, 100%'
+                  sizes="(max-width: 767px) 375px, 100%"
                   type="image/webp"
-                  loading='lazy'
-                  fetchpriority={idx === 0 ? "high" : "low"}
+                  loading={idx === 0 ? 'eager' : 'lazy'}
+                  fetchpriority={idx === 0 ? 'high' : 'low'}
                 />
                 <source
                   srcSet={`${item.tablet.webp} 1x, ${item.tablet.webp_2x} 2x`}
                   media="(min-width: 768px) and (max-width: 1439px)"
-                  sizes='(min-width: 768px) and (max-width: 1439px) 768px, 100%'
+                  sizes="(min-width: 768px) and (max-width: 1439px) 768px, 100%"
                   type="image/webp"
-                  loading='lazy'
-                  fetchpriority={idx === 0 ? "high" : "low"}
+                  loading={idx === 0 ? 'eager' : 'lazy'}
+                  fetchpriority={idx === 0 ? 'high' : 'low'}
                 />
                 <source
                   srcSet={`${item.desktop.webp} 1x, ${item.desktop.webp_2x} 2x`}
                   media="(min-width: 1440px) "
-                  sizes='(min-width: 1440px) 1440px, 100%'
-                  loading='lazy'
+                  sizes="(min-width: 1440px) 1440px, 100%"
+                  loading={idx === 0 ? 'eager' : 'lazy'}
                   type="image/webp"
-                  fetchpriority={idx === 0 ? "high" : "low"}
+                  fetchpriority={idx === 0 ? 'high' : 'low'}
                 />
                 {/* <source
                   srcSet={`${item.imgWebP1} 1x, ${item.imgWebP2} 2x`}
@@ -77,7 +77,13 @@ const HeroSection = () => {
                   type="image/png"
                   fetchpriority={idx === 0 ? "high" : "low"}
                 /> */}
-                <img fetchpriority={idx === 0 ? "high" : "low"} className="hero-img" src={item.desktop.png} alt={item.alt} loading='lazy'/>
+                <img
+                  fetchpriority={idx === 0 ? 'high' : 'low'}
+                  loading={idx === 0 ? 'eager' : 'lazy'}
+                  className="hero-img"
+                  src={item.desktop.png}
+                  alt={item.alt}
+                />
               </picture>
             </SwiperSlide>
           ))}
