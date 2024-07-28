b52992bbe095c6d60959a5bc7d82f54e8e01f096
WorldThirteen <mihailzachepilo@gmail.com>
2024-07-28 10:50:27 +0000

    Deploying to gh-pages from @ Poltava-mistohub/MistoHub@0d374c8ee63b255c5fdf40cdb6ba1c85d3f7ab11 🚀

--- assets/InvestorsBlock-857ba860.js
+++ assets/InvestorsBlock-857ba860.js
@@ -1,4 +1,4 @@
-import{u as h,j as t,f as r,h as A,r as x}from"./index-b508e5da.js";import{M,I as $,u as j,n as y,C as W,g as N}from"./HomePage-015c53c6.js";const I=h.div`
+import{u as h,j as t,f as r,h as W,r as x}from"./index-f4468b84.js";import{M,I as j,u as y,C,g as k}from"./HomePage-e9a2e9e2.js";const I=h.div`
   .text {
     color: rgba(11, 11, 11, 0.5);
     font-size: 12px;
@@ -248,7 +248,7 @@ import{u as h,j as t,f as r,h as A,r as x}from"./index-b508e5da.js";import{M,I a
       height: 100px;
     }
   }
-`,C=h(M)`
+`,T=h(M)`
   &.ReactModal__Content {
     width: 100%;
     height: fit-content;
@@ -274,7 +274,7 @@ import{u as h,j as t,f as r,h as A,r as x}from"./index-b508e5da.js";import{M,I a
       width: 576px;
     }
   }
-`,T="/MistoHub/assets/noname-a8edc818.jpg",G=({data:e,onClose:n,isOpen:i})=>{const{firstName:a,secondName:s,type:l,imageURL:d,facebook:o,instagram:p,question:u,answer:b}=e;return t.jsx(C,{onRequestClose:n,isOpen:i,style:{overlay:{zIndex:"2"}},onClose:n,shouldCloseOnOverlayClick:!0,contentLabel:"Image Modal",children:t.jsx(I,{children:t.jsxs("div",{className:"contentWrapper",children:[t.jsxs("div",{className:"top-wrapper",children:[t.jsxs("div",{className:"title-wrapper",children:[t.jsx("img",{className:"main-image",src:d||T,alt:`${a} ${s}`,loading:"lazy"}),t.jsx("div",{className:"svgWrapper",children:t.jsx("a",{href:o||p,target:"_blank",rel:"noopener noreferrer",children:o?t.jsx($,{width:"28",height:"28",iconName:"icon-faceb",styles:"instaIcon"}):t.jsx($,{width:"28",height:"28",iconName:"icon-instagram",styles:"instaIcon"})})})]}),t.jsxs("div",{className:"main-heading-wrapper",children:[t.jsx("h2",{className:"companyName",children:`${a} ${s}`}),t.jsx("p",{className:"text",children:l})]}),t.jsxs("div",{className:"closeWrapper",children:[t.jsx("button",{className:"closeWord",onClick:n,children:"Закрити"}),t.jsx("button",{className:"closeButton",onClick:n,children:t.jsx($,{width:"12",height:"12",iconName:"close",styles:"close-icon"})})]})]}),t.jsx("p",{className:"textQuestion",children:u}),t.jsx("p",{className:"textAnswer",children:b})]})})})},f=({startTranslate:e,endTranslate:n})=>A`
+`,G="/MistoHub/assets/noname-a8edc818.jpg",L=({data:e,onClose:n,isOpen:i})=>{const{firstName:a,secondName:s,type:l,imageURL:d,facebook:o,instagram:p,question:f,answer:m}=e;return t.jsx(T,{onRequestClose:n,isOpen:i,style:{overlay:{zIndex:"2"}},onClose:n,shouldCloseOnOverlayClick:!0,contentLabel:"Image Modal",children:t.jsx(I,{children:t.jsxs("div",{className:"contentWrapper",children:[t.jsxs("div",{className:"top-wrapper",children:[t.jsxs("div",{className:"title-wrapper",children:[t.jsx("img",{className:"main-image",src:d||G,alt:`${a} ${s}`,loading:"lazy"}),t.jsx("div",{className:"svgWrapper",children:t.jsx("a",{href:o||p,target:"_blank",rel:"noopener noreferrer",children:o?t.jsx(j,{width:"28",height:"28",iconName:"icon-faceb",styles:"instaIcon"}):t.jsx(j,{width:"28",height:"28",iconName:"icon-instagram",styles:"instaIcon"})})})]}),t.jsxs("div",{className:"main-heading-wrapper",children:[t.jsx("h2",{className:"companyName",children:`${a} ${s}`}),t.jsx("p",{className:"text",children:l})]}),t.jsxs("div",{className:"closeWrapper",children:[t.jsx("button",{className:"closeWord",onClick:n,children:"Закрити"}),t.jsx("button",{className:"closeButton",onClick:n,children:t.jsx(j,{width:"12",height:"12",iconName:"close",styles:"close-icon"})})]})]}),t.jsx("p",{className:"textQuestion",children:f}),t.jsx("p",{className:"textAnswer",children:m})]})})})},u=({startTranslate:e,endTranslate:n})=>W`
   0% {
     transform: translateX(${e}%);
   }
@@ -307,7 +307,7 @@ import{u as h,j as t,f as r,h as A,r as x}from"./index-b508e5da.js";import{M,I a
     max-width: 1440px;
   }
 `,D=h.div`
-  ${e=>{const n={startTranslate:0,endTranslate:e.$length===20?-(e.$length*e.$length-.3*e.$length):-(e.$length*e.$length+.3*e.$length)},i={startTranslate:e.$length===20?-(e.$length*e.$length-.3*e.$length):-(e.$length*e.$length+.3*e.$length),endTranslate:0},a=f(n),s=f(i),l={startTranslate:0,endTranslate:e.$length===20?-(e.$length*e.$length+2.2*e.$length):-(e.$length*e.$length+.2*e.$length)},d={startTranslate:e.$length===20?-(e.$length*e.$length+2.2*e.$length):-(e.$length*e.$length+.2*e.$length),endTranslate:0},o=f(l),p=f(d),u={startTranslate:0,endTranslate:e.$length===20?-(e.$length*e.$length-10.5*e.$length):-(e.$length*e.$length+1.2*e.$length)},b={startTranslate:e.$length===20?-(e.$length*e.$length-10.5*e.$length):-(e.$length*e.$length-8*e.$length),endTranslate:0},w=f(u),c=f(b);return r`
+  ${e=>{const n={startTranslate:0,endTranslate:e.$length===20?-(e.$length*e.$length-.3*e.$length):-(e.$length*e.$length+.3*e.$length)},i={startTranslate:e.$length===20?-(e.$length*e.$length-.3*e.$length):-(e.$length*e.$length+.3*e.$length),endTranslate:0},a=u(n),s=u(i),l={startTranslate:0,endTranslate:e.$length===20?-(e.$length*e.$length+2.2*e.$length):-(e.$length*e.$length+.2*e.$length)},d={startTranslate:e.$length===20?-(e.$length*e.$length+2.2*e.$length):-(e.$length*e.$length+.2*e.$length),endTranslate:0},o=u(l),p=u(d),f={startTranslate:0,endTranslate:e.$length===20?-(e.$length*e.$length-10.5*e.$length):-(e.$length*e.$length+1.2*e.$length)},m={startTranslate:e.$length===20?-(e.$length*e.$length-10.5*e.$length):-(e.$length*e.$length-8*e.$length),endTranslate:0},b=u(f),w=u(m);return r`
       &.group-0 {
         overflow: visible;
         animation: ${r`
@@ -394,37 +394,37 @@ import{u as h,j as t,f as r,h as A,r as x}from"./index-b508e5da.js";import{M,I a
         &.group-0 {
           overflow: visible;
           animation: ${r`
-            ${w} 80s linear infinite
+            ${b} 80s linear infinite
           `};
         }
         &.group-1 {
           overflow: visible;
           animation: ${r`
-            ${c} 80s linear infinite
+            ${w} 80s linear infinite
           `};
         }
         &.group-2 {
           overflow: visible;
           animation: ${r`
-            ${w} 80s linear infinite
+            ${b} 80s linear infinite
           `};
         }
         &.group-3 {
           overflow: visible;
           animation: ${r`
-            ${c} 80s linear infinite
+            ${w} 80s linear infinite
           `};
         }
         &.group-4 {
           overflow: visible;
           animation: ${r`
-            ${w} 80s linear infinite
+            ${b} 80s linear infinite
           `};
         }
         &.group-5 {
           overflow: visible;
           animation: ${r`
-            ${c} 80s linear infinite
+            ${w} 80s linear infinite
           `};
         }
       }
@@ -503,14 +503,14 @@ import{u as h,j as t,f as r,h as A,r as x}from"./index-b508e5da.js";import{M,I a
       cursor: pointer;
     }
   }
-`,X=(e,n,i,a,s,l)=>{const d=e%2===0,o=e===(l==null?void 0:l.length)-1;let p=!1;return d?p=e===0||o?n.left<=i.left+(a?40:s?80:200)||n.left>=i.right-(a?80:s?180:400):n.left<=i.left+(a?-30:s?-40:10)||n.left>=i.right-(a?50:s?90:170):p=o?n.right<=i.left+(a?80:s?180:400)||n.right>=i.right-(a?40:s?80:200):n.right<=i.left+(a?50:s?90:170)||n.right>=i.right-(a?-30:s?-40:10),p},U=e=>{const n=[];for(let i=0;i<e.length;i+=20){let a=e.slice(i,i+20);if(a.length<20&&i+20>=e.length){const s=20-a.length;a=[...a,...Array.from({length:s},(l,d)=>a[d%a.length])]}n.push(a)}return n},q=({people:e})=>{const[n,i]=x.useState(null),[a,s]=x.useState(null),[l,d]=x.useState(!1),o=j({maxWidth:767}),p=j({maxWidth:1440});x.useEffect(()=>{(async()=>{try{const g=U(e);i(g)}catch(g){console.error("Error fetching people data:",g)}})()},[e]);const u=x.useMemo(()=>c=>{const g=document.getElementById("investors-anim"),O=document.getElementById(`container-${c}`).getElementsByClassName("item-list");for(const v of O){const R=v.getBoundingClientRect(),S=g.getBoundingClientRect();X(c,R,S,o,p,n)?(v.classList.add("on-blur"),v.disabled=!0):(v.classList.remove("on-blur"),v.disabled=!1)}},[o,p,n]);x.useEffect(()=>{if(!n)return;const c=setInterval(()=>{n.forEach((g,m)=>{u(m)})},10);return()=>clearInterval(c)},[n,u]);const b=c=>{s(c),d(!0)},w=()=>{s(null),d(!1)};return t.jsxs(Y,{children:[t.jsx(B,{id:"investors-anim",children:n==null?void 0:n.map((c,g)=>t.jsx(D,{id:`container-${g}`,$length:c.length,className:`group-${g} ${g%2===0?"odd-group":"even-group"}`,children:t.jsx("ul",{children:c.map(m=>t.jsx("li",{children:t.jsx("button",{"data-group_id":g,className:"item-list",type:"button",onClick:()=>b(m),children:t.jsx("img",{src:m.imageURL?m.imageURL:T,alt:`Investor ${m.id}`,loading:"lazy"})})},y()))})},y()))}),a&&t.jsx(G,{data:a,onClose:w,isOpen:l})]})},L="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAANsAAACkCAAAAAA7nASIAAACrklEQVR42u3cu26kMBSA4bz/S6GVCwoKiikopnBB4YLCBQWFC5axxzeGTBJlR3sY/acJHIHCp+O7onys7xsf2LBhw4YNGzZs2LBhw4YNGzZs2LBhw4YNGzZsb2378yRe/nm/+t3YsGETZ5M1AGLDhg0bNmzYsGHDhg0bNmzYsGF7ke28gQ0bNmzYsGHDhg2bYJsdtR7n47R9SM9Ga22dLJttttD7rNOq8dHqw7TSFcO0Id0Mi3jbfCfcosufO7dFOpfU9TmtJuG2krZ9rnuedl2ZbibRNucN7Wjt1V/193So2qDNtS3Sq6+aulo7+rRaJNsunlDUxPhr7cW+Kbohp41voaGIPn0RbLNlUWYPStVUsZd1CdGWtfKVm+XahqrXXOLdWD043e5S2YY0YFZ38my+PuvD1/pulUf+Xhub7Gm+c6nMIm1z2STXdfHdabuIP3ehYgFzW20WqTazSzTh4+dPhokduS6jNJveJbrQFo/XL/XAE98ez2LrQyWK53QM+4lNn9eWliD6LWzuRzaxbXJ6Mpb0D7ZzjSW7OcCVc0CYuewt4mu7OaAXPQfs5u4pzt31zOXia7tCNdXLwtdcw/GaK82C51pz2bIL5bXyUm7awn5Hp4XJUqYFr5VDnxmK7ajJFQybGXdJY0kYGe/pbjdqSrDl6BNo25tOQ1N87OLTajDmqvI4ee+It71pSEvbm9a20GvyyYgrO1N6sI22+qjhH0wAL7WtRh3QKlznxtSUyyMiNa/Cbesy3O9bUx0SxROtq7vNAvqLoz2pZ6/Oboth81CFZdrSk/vukaxM2/8NbNiwYcOGDRs2bNhea+PvzLFhw4YNGzZs2LBhw4YNGzZs2LBhO4GN/xuKDRu2X9rOG9iwYcOGDRs2bNiwYcOGDRs2bNiwYcOGDRu2EH8B4ajrnFKix8YAAAAASUVORK5CYII=",Q=({isOpen:e,data:n,onClose:i})=>{const{name:a,logoURL:s,link:l,question:d,answer:o}=n;return e?t.jsx(C,{onRequestClose:i,isOpen:e,style:{overlay:{zIndex:"2"}},onClose:i,shouldCloseOnOverlayClick:!0,contentLabel:"Image Modal",children:t.jsx(I,{children:t.jsxs("div",{className:"contentWrapper",children:[t.jsxs("div",{className:"top-wrapper",children:[t.jsxs("div",{className:"title-wrapper",children:[t.jsx("img",{className:"main-image",src:s||L,alt:a,loading:"lazy"}),l&&t.jsx("div",{className:"svgWrapper",children:t.jsx("a",{href:l,target:"_blank",rel:"noopener noreferrer",children:t.jsx($,{iconName:"icon-instagram",styles:"instaIcon"})})})]}),t.jsxs("div",{className:"main-heading-wrapper",children:[t.jsx("h2",{className:"companyName",children:a}),t.jsx("p",{className:"text",children:"компанія"})]}),t.jsxs("div",{className:"closeWrapper",children:[t.jsx("button",{className:"closeWord",onClick:i,children:"Закрити"}),t.jsx("button",{className:"closeButton",onClick:i,children:t.jsx($,{iconName:"close",styles:"close-icon",width:12,height:12})})]})]}),t.jsx("p",{className:"textQuestion",children:d}),t.jsx("p",{className:"textAnswer",children:o})]})})}):null},Z=({start:e,end:n})=>A`
+`,X=(e,n,i,a,s,l)=>{const d=e%2===0,o=e===(l==null?void 0:l.length)-1;let p=!1;return d?p=e===0||o?n.left<=i.left+(a?40:s?80:200)||n.left>=i.right-(a?80:s?180:400):n.left<=i.left+(a?-30:s?-40:10)||n.left>=i.right-(a?50:s?90:170):p=o?n.right<=i.left+(a?80:s?180:400)||n.right>=i.right-(a?40:s?80:200):n.right<=i.left+(a?50:s?90:170)||n.right>=i.right-(a?-30:s?-40:10),p},U=e=>{const n=[];for(let i=0;i<e.length;i+=20){let a=e.slice(i,i+20);if(a.length<20&&i+20>=e.length){const s=20-a.length;a=[...a,...Array.from({length:s},(l,d)=>a[d%a.length])]}n.push(a)}return n},q=e=>[(e.firstName||"").trim(),(e.secondName||"").trim()].join(" "),Q=({people:e})=>{const[n,i]=x.useState(null),[a,s]=x.useState(null),[l,d]=x.useState(!1),o=y({maxWidth:767}),p=y({maxWidth:1440}),f={background:`rgb(212,213,209) center / contain no-repeat url(${G})`};x.useEffect(()=>{(async()=>{try{const c=U(e);i(c)}catch(c){console.error("Error fetching people data:",c)}})()},[e]);const m=x.useMemo(()=>g=>{const c=document.getElementById("investors-anim"),N=document.getElementById(`container-${g}`).getElementsByClassName("item-list");for(const $ of N){const R=$.getBoundingClientRect(),S=c.getBoundingClientRect();X(g,R,S,o,p,n)?($.classList.add("on-blur"),$.disabled=!0):($.classList.remove("on-blur"),$.disabled=!1)}},[o,p,n]);x.useEffect(()=>{if(!n)return;const g=setInterval(()=>{for(let c=0;c<n.length;c++)m(c)},50);return()=>clearInterval(g)},[n,m]);const b=g=>{s(g),d(!0)},w=()=>{s(null),d(!1)};return t.jsxs(Y,{children:[t.jsx(B,{id:"investors-anim",children:n==null?void 0:n.map((g,c)=>t.jsx(D,{id:`container-${c}`,$length:g.length,className:`group-${c} ${c%2===0?"odd-group":"even-group"}`,children:t.jsx("ul",{children:g.map((v,N)=>t.jsx("li",{children:t.jsx("button",{"data-group_id":c,className:"item-list",type:"button",onClick:()=>b(v),children:t.jsx("img",{alt:"",title:q(v),src:v.imageURL?v.imageURL:null,style:f,loading:"lazy"})})},N))})},`group-${c}`))}),a&&t.jsx(L,{data:a,onClose:w,isOpen:l})]})},O="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAANsAAACkCAAAAAA7nASIAAACrklEQVR42u3cu26kMBSA4bz/S6GVCwoKiikopnBB4YLCBQWFC5axxzeGTBJlR3sY/acJHIHCp+O7onys7xsf2LBhw4YNGzZs2LBhw4YNGzZs2LBhw4YNGzZsb2378yRe/nm/+t3YsGETZ5M1AGLDhg0bNmzYsGHDhg0bNmzYsGF7ke28gQ0bNmzYsGHDhg2bYJsdtR7n47R9SM9Ga22dLJttttD7rNOq8dHqw7TSFcO0Id0Mi3jbfCfcosufO7dFOpfU9TmtJuG2krZ9rnuedl2ZbibRNucN7Wjt1V/193So2qDNtS3Sq6+aulo7+rRaJNsunlDUxPhr7cW+Kbohp41voaGIPn0RbLNlUWYPStVUsZd1CdGWtfKVm+XahqrXXOLdWD043e5S2YY0YFZ38my+PuvD1/pulUf+Xhub7Gm+c6nMIm1z2STXdfHdabuIP3ehYgFzW20WqTazSzTh4+dPhokduS6jNJveJbrQFo/XL/XAE98ez2LrQyWK53QM+4lNn9eWliD6LWzuRzaxbXJ6Mpb0D7ZzjSW7OcCVc0CYuewt4mu7OaAXPQfs5u4pzt31zOXia7tCNdXLwtdcw/GaK82C51pz2bIL5bXyUm7awn5Hp4XJUqYFr5VDnxmK7ajJFQybGXdJY0kYGe/pbjdqSrDl6BNo25tOQ1N87OLTajDmqvI4ee+It71pSEvbm9a20GvyyYgrO1N6sI22+qjhH0wAL7WtRh3QKlznxtSUyyMiNa/Cbesy3O9bUx0SxROtq7vNAvqLoz2pZ6/Oboth81CFZdrSk/vukaxM2/8NbNiwYcOGDRs2bNhea+PvzLFhw4YNGzZs2LBhw4YNGzZs2LBhO4GN/xuKDRu2X9rOG9iwYcOGDRs2bNiwYcOGDRs2bNiwYcOGDRu2EH8B4ajrnFKix8YAAAAASUVORK5CYII=",Z=({isOpen:e,data:n,onClose:i})=>{const{name:a,logoURL:s,link:l,question:d,answer:o}=n;return e?t.jsx(T,{onRequestClose:i,isOpen:e,style:{overlay:{zIndex:"2"}},onClose:i,shouldCloseOnOverlayClick:!0,contentLabel:"Image Modal",children:t.jsx(I,{children:t.jsxs("div",{className:"contentWrapper",children:[t.jsxs("div",{className:"top-wrapper",children:[t.jsxs("div",{className:"title-wrapper",children:[t.jsx("img",{className:"main-image",src:s||O,alt:a,loading:"lazy"}),l&&t.jsx("div",{className:"svgWrapper",children:t.jsx("a",{href:l,target:"_blank",rel:"noopener noreferrer",children:t.jsx(j,{iconName:"icon-instagram",styles:"instaIcon"})})})]}),t.jsxs("div",{className:"main-heading-wrapper",children:[t.jsx("h2",{className:"companyName",children:a}),t.jsx("p",{className:"text",children:"компанія"})]}),t.jsxs("div",{className:"closeWrapper",children:[t.jsx("button",{className:"closeWord",onClick:i,children:"Закрити"}),t.jsx("button",{className:"closeButton",onClick:i,children:t.jsx(j,{iconName:"close",styles:"close-icon",width:12,height:12})})]})]}),t.jsx("p",{className:"textQuestion",children:d}),t.jsx("p",{className:"textAnswer",children:o})]})})}):null},F=({start:e,end:n})=>W`
   0% {
     transform: translateX(${e});
   }
   100% {
     transform: translateX(${n}px);
   }
-`,F=h.div`
+`,K=h.div`
   margin-bottom: 60px;
   overflow: hidden;
 
@@ -521,8 +521,8 @@ import{u as h,j as t,f as r,h as A,r as x}from"./index-b508e5da.js";import{M,I a
   @media screen and (min-width: 1440px) {
     margin-bottom: 150px;
   }
-`,K=h.ul`
-  ${e=>{if(!e.$anim)return;const n=Z(e.$anim);return r`
+`,P=h.ul`
+  ${e=>{if(!e.$anim)return;const n=F(e.$anim);return r`
       animation: ${n} ${e.$anim.time}s linear infinite;
     `}}
 
@@ -559,7 +559,7 @@ import{u as h,j as t,f as r,h as A,r as x}from"./index-b508e5da.js";import{M,I a
       animation-play-state: paused;
     }
   }
-`,P=(e,n,i)=>{if(!e)return;const a=e*(n||i?212:92),s=n?1440:i?768:375,l=a*2-s,d=n?34.2:i?68.2:29.6;return{start:0,end:-l,time:l/d}},H=({companies:e})=>{const[n,i]=x.useState(null),a=j({minWidth:1440}),s=j({minWidth:768}),l=o=>{i(o)},d=()=>{i(null)};if(e)return t.jsx(F,{children:Array.isArray(e)&&t.jsxs(K,{$anim:P(e.length,a,s),children:[Array.from([...e,...e]).map(o=>t.jsx("li",{children:t.jsx("img",{src:o.logoURL||L,alt:`Company ${o.id}`,onClick:()=>l(o),loading:"lazy"})},y())),n&&t.jsx(Q,{isOpen:n!==null,data:n,onClose:d})]})})},k=h(W)`
+`,H=(e,n,i)=>{if(!e)return;const a=e*(n||i?212:92),s=n?1440:i?768:375,l=a*2-s,d=n?34.2:i?68.2:29.6;return{start:0,end:-l,time:l/d}},J=({companies:e})=>{const[n,i]=x.useState(null),a=y({minWidth:1440}),s=y({minWidth:768}),l=o=>{i(o)},d=()=>{i(null)};if(e)return t.jsx(K,{children:Array.isArray(e)&&t.jsxs(P,{$anim:H(e.length,a,s),children:[Array.from([...e,...e]).map((o,p)=>t.jsx("li",{children:t.jsx("img",{src:o.logoURL||O,alt:`Company ${o.id}`,title:o.name,onClick:()=>l(o),loading:"lazy"})},p)),n&&t.jsx(Z,{isOpen:n!==null,data:n,onClose:d})]})})},z=h(C)`
   padding-top: 60px;
   margin-bottom: 32px;
 
@@ -572,9 +572,9 @@ import{u as h,j as t,f as r,h as A,r as x}from"./index-b508e5da.js";import{M,I a
     padding-top: 150px;
     margin-bottom: 80px;
   }
-`,J=h(W)`
+`,E=h(C)`
   padding: 0;
-`,E=h.p`
+`,_=h.p`
   color: rgba(11, 11, 11, 0.5);
   font-size: 14px;
   line-height: 18px;
@@ -591,7 +591,7 @@ import{u as h,j as t,f as r,h as A,r as x}from"./index-b508e5da.js";import{M,I a
     line-height: 24px;
     letter-spacing: -0.32px;
   }
-`,_=h.h2`
+`,V=h.h2`
   color: #0b0b0b;
   font-family: Oddval;
   font-size: 24px;
@@ -611,7 +611,7 @@ import{u as h,j as t,f as r,h as A,r as x}from"./index-b508e5da.js";import{M,I a
     line-height: 68px;
     letter-spacing: -1.2px;
   }
-`,z=h.p`
+`,A=h.p`
   color: rgba(11, 11, 11, 0.8);
   font-size: 16px;
   line-height: 20px;
@@ -628,4 +628,4 @@ import{u as h,j as t,f as r,h as A,r as x}from"./index-b508e5da.js";import{M,I a
     line-height: 34px;
     letter-spacing: -0.56px;
   }
-`,ne=({people:e,companies:n})=>{const[i,a]=x.useState(null),s=()=>{a(null)};return t.jsxs("section",{id:"investors",children:[t.jsxs(k,{children:[t.jsx(E,{children:"Імпакт-інвестори МІСТОХАБ"}),t.jsx(_,{children:"З нами вже"}),(e==null?void 0:e.length)>0&&t.jsxs(z,{children:[e.length," ",N(e.length,"p")]})]}),t.jsxs(J,{children:[e&&t.jsx(q,{people:e}),t.jsx(k,{children:(n==null?void 0:n.length)>0&&t.jsxs(z,{children:[n.length," ",N(n.length,"k")]})}),n&&t.jsx(H,{companies:n})]}),i&&t.jsx(G,{data:i,onClose:s})]})};export{ne as default};
+`,ie=({people:e,companies:n})=>{const[i,a]=x.useState(null),s=()=>{a(null)};return t.jsxs("section",{id:"investors",children:[t.jsxs(z,{children:[t.jsx(_,{children:"Імпакт-інвестори МІСТОХАБ"}),t.jsx(V,{children:"З нами вже"}),(e==null?void 0:e.length)>0&&t.jsxs(A,{children:[e.length," ",k(e.length,"p")]})]}),t.jsxs(E,{children:[e&&t.jsx(Q,{people:e}),t.jsx(z,{children:(n==null?void 0:n.length)>0&&t.jsxs(A,{children:[n.length," ",k(n.length,"k")]})}),n&&t.jsx(J,{companies:n})]}),i&&t.jsx(L,{data:i,onClose:s})]})};export{ie as default};
