da4ab4daa347ea977f10e12603647baf7762fdcd
Mykhailo Zachepylo <mihailzachepilo@gmail.com>
2024-07-28 23:09:30 +0300

    Move HeroPicture into seprate component, so could be reused

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -8,6 +8,7 @@ import { heroData } from '/src/constants/dataHeroSwiper.js';
 import { HeroSectionStyled } from '/src/components/HeroSection/HeroSection.styled';
 import { HeroContainer } from '/src/components/HeroSection/HeroSection.styled';
 import Icon from '/src/components/Icon/Icon';
+import { HeroPicture } from './HeroPicture';
 
 const HeroSection = () => {
   const swiperRef = useRef(null);
@@ -40,34 +41,9 @@ const HeroSection = () => {
       <HeroContainer>
         <Swiper ref={swiperRef} className="my-swiper" navigation={true}>
           {heroData.map((item, idx) => (
-            <SwiperSlide key={nanoid()}>
-              <picture>
-                <source
-                  srcSet={`${item.mobile.webp} 375w, ${item.mobile.webp_2x} 750w`}
-                  media="(max-width: 767px)"
-                  sizes="(max-width: 767px) 375px, 100%"
-                  type="image/webp"
-                />
-                <source
-                  srcSet={`${item.tablet.webp} 768w, ${item.tablet.webp_2x} 1536w`}
-                  media="(min-width: 768px) and (max-width: 1439px)"
-                  sizes="(min-width: 768px) and (max-width: 1439px) 768px, 100%"
-                  type="image/webp"
-                />
-                <source
-                  srcSet={`${item.desktop.webp} 1440w, ${item.desktop.webp_2x} 2880w`}
-                  media="(min-width: 1440px)"
-                  sizes="(min-width: 1440px) 1440px, 100%"
-                  type="image/webp"
-                />
-                <img
-                  fetchpriority={idx === 0 ? 'high' : 'low'}
-                  loading={idx === 0 ? 'eager' : 'lazy'}
-                  className="hero-img"
-                  src={item.desktop.png}
-                  alt={item.alt}
-                />
-              </picture>
+            // Slide should not be re-mounted sho key should be stale
+            <SwiperSlide key={idx}>
+              <HeroPicture item={item} priority={idx === 0} />
             </SwiperSlide>
           ))}
         </Swiper>
